ed8d05b27e8368c4bb31e0a49f57a100b87cf8cc
Abbey code <92787850+Harbiodun-coder@users.noreply.github.com>
2022-03-04 10:25:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var length = prompt("enter your length")
+var breadth = prompt("enter your breadth")
+var Area = (length*breadth)
+alert(`The area is ${Area}`)
+document.write(`The area of the Rectangle is ${Area}`)
+console.log(`The area of the Rectangle is ${Area}`)
+
+console.log(`Baba Basira is a man, his name is Jubril Musa. He is a man that likes women a lot, which has resulted in him having 70 children,he is currently married to 4 women.
+           He is rich enough to rival Dangote, he currently owns 6 cars of varying worths, he owns 3 different estates. 
+           His greatest strength is his Flashlight , while his greatest weakness is food. 
+           His favourite Person is currently his flashlight.`)
+           
+alert(`Baba Basira is a man, his name is Jubril Musa. He is a man that likes women a lot, which has resulted in him having 70 children,he is currently married to 4 women.
+He is rich enough to rival Dangote, he currently owns 6 cars of varying worths, he owns 3 different estates. 
+His greatest strength is his Flashlight , while his greatest weakness is food. 
+His favourite Person is currently his flashlight.`)
+
+
+alert(`Baba Basira is a man, his name is Jubril Musa. He is a man that likes women a lot, which has resulted in him having 70 children,he is currently married to 4 women.
+He is rich enough to rival Dangote, he currently owns 6 cars of varying worths, he owns 3 different estates. 
+His greatest strength is his Flashlight , while his greatest weakness is food. 
+His favourite Person is currently his flashlight.`)
+
+
+var food = prompt("what your favorite food")
+var taste = prompt("How does it taste")
+var ingredients = prompt("What do you need to make it")
+var prepare = prompt("Where did you learn to prepare it")
+var foodie = { food,taste,prepare, ingredients  }
+alert(`My favorite food is ${food}, it is ${taste},i learned how to prepare it from ${prepare}
+      the ingredients required are ${ingredients}`)
+document.write(`My favorite food is ${food}, it is ${taste},i learned how to prepare it from ${prepare}
+the ingredients required are ${ingredients}`)
+console.log(`My favorite food is ${food}, it is ${taste},i learned how to prepare it from ${prepare}
+the ingredients required are ${ingredients}`)
